f402272a9a6b8bb205c3662848215b5f9fd8a8bd
Saif AlAbabseh <sababseh97@gmail.com>
2023-01-15 10:02:17 +0300

    Upload all project files.

--- DBInfo.java
+++
@@ -1,7 +0,0 @@
-package all_chat;
-
-public class DBInfo {
-	public static String db = "jdbc:mysql://localhost/allchat";
-	public static String user = "root";
-	public static String pass = "12345678";
-}
